ae8433a6af3f753c83da5460032f6fb2f6a5b99b
sofimes <sofanitmesfin19@gmail.com>
2025-11-04 14:07:09 +0300

    add list and get product routes

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -1,6 +1,15 @@
 const express = require("express");
 const router = express.Router();
-const { syncProduct } = require("../controllers/ProductController");
+const {
+  syncProduct,
+  listProducts,
+  getProductByASIN,
+} = require("../controllers/ProductController");
 
 router.post("/sync", syncProduct);
+
+router.get("/", listProducts);
+
+router.get("/:asin", getProductByASIN);
+
 module.exports = router;
