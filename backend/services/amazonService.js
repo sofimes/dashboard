de5f4fc19805590e580113270b9e52a75290c0cc
sofimes <sofanitmesfin19@gmail.com>
2025-11-04 14:06:18 +0300

    rainforest api integration

--- backend/services/amazonService.js
+++ backend/services/amazonService.js
@@ -13,18 +13,36 @@ async function fetchProductByASIN(asin) {
         amazon_domain: "amazon.com",
         asin,
       },
+      // timeout: 15000,
     });
 
     const data = response.data.product;
+    if (!data) {
+      console.error("Product not found in Rainforest response:", response.data);
+      throw new Error("Product not found");
+    }
+    let price = 0;
+    let currency = "USD";
 
+    if (data.buybox_price) {
+      price = data.buybox_price.value;
+      currency = data.buybox_price.currency || "USD";
+    } else if (data.variants && data.variants.length > 0) {
+      const currentVariant =
+        data.variants.find((v) => v.is_current_product) || data.variants[0];
+      if (currentVariant?.price) {
+        price = currentVariant.price.value || 0;
+        currency = currentVariant.price.currency || "USD";
+      }
+    }
     const [product, created] = await Product.upsert(
       {
         asin: data.asin,
         title: data.title || "",
         brand: data.brand || "",
         category: data.categories_flat || "",
-        price: data.buybox_price?.value || 0,
-        currency: data.buybox_price?.currency || "USD",
+        price,
+        currency,
         image_url: data.main_image?.link || "",
         product_url: data.link || "",
         rating: data.rating || 0,
@@ -36,7 +54,7 @@ async function fetchProductByASIN(asin) {
     );
     return product;
   } catch (error) {
-    console.error("Error fetching product", error.message);
+    console.error("Error fetching product here", error.message);
     throw error;
   }
 }
